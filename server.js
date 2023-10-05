d7dc03a379299200033bd37b6faa7176d18d803b
SaileshRokaya <saileshrokaya123@gmail.com>
2023-10-06 00:52:08 +0545

    Fixed: CRUD Operation

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get("/api/contacts", (req, res) => {
-    res.send("Get all contacts");
-});
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 app.listen(port, ()=>{
     console.log(`Server runnning on port ${port}`);
